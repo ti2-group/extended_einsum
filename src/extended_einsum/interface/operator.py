e2ccea8fef57307f89e0ebcc82c7eaa95dfdecc4
eichelb4rt <maurice.wenig@gmail.com>
2026-06-17 01:20:14 +0200

    refractored interface

---
+++ src/extended_einsum/interface/operator.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+
+from extended_einsum.language import BinaryOperator, UnaryOperator
+
+
+@dataclass(frozen=True)
+class InterfaceUnaryOperator:
+    operator: UnaryOperator
+
+
+@dataclass(frozen=True)
+class InterfaceBinaryOperator:
+    operator: BinaryOperator
+
+
+@dataclass(frozen=True)
+class InterfaceEinsumOperator:
+    format_string: str
+
+
+@dataclass(frozen=True)
+class InterfaceStackOperator:
+    axis: int
+
+
+@dataclass(frozen=True)
+class InterfaceTakeOperator:
+    axis: int
+
+
+@dataclass(frozen=True)
+class InterfaceSoftmaxOperator:
+    axis: int
+
+
+# @dataclass(frozen=True)
+# class InterfaceSumOperator:
+#     axis: int
+
+
+@dataclass(frozen=True)
+class InterfaceSliceOperator:
+    start: int
+    stop: int
+    axis: int
+
+
+InterfaceOperator = (
+    InterfaceUnaryOperator
+    | InterfaceBinaryOperator
+    | InterfaceEinsumOperator
+    | InterfaceStackOperator
+    | InterfaceTakeOperator
+    | InterfaceSoftmaxOperator
+    | InterfaceSliceOperator
+)
